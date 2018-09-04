ac6bda667df2ffca01b9bddd848440423ec721e8
Kadu França <40800067+kadu-franca@users.noreply.github.com>
2018-09-04 11:59:00 -0300

    Update tabelas.sql
    corrigimos as referencias das chaves estrangeiras

--- tabelas.sql
+++ tabelas.sql
@@ -1,75 +1,80 @@
 CREATE TABLE Usuario (
 
 		id_usuario INT NOT NULL,
-		login STRING,
-		nome STRING,
-		senha STRING,
-		email STRING,
-		tel STRING,
+		login VARCHAR(100),
+		nome VARCHAR(100),
+		senha VARCHAR(20),
+		email VARCHAR(100),
+		tel VARCHAR(50),
 		PRIMARY KEY (id_usuario),
-		FOREIGN KEY (id_usuario) REFERENCES 
+		FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
 );
 
 
 CREATE TABLE Turma (
 
 		id_turma INT NOT NULL,
-		nome STRING,
-		turno STRING,
-		serie STRING,
+		id_usuario INT,
+		nome VARCHAR(100),
+		turno VARCHAR(50),
+		serie VARCHAR(50),
 		PRIMARY KEY (id_turma),
-		FOREIGN KEY (id_turma) REFERENCES Turma(id_turma)
+		FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
 );
 
 
 CREATE TABLE Professor (
 
 		id_professor INT NOT NULL,
-		siape STRING,
+		id_usuario INT,
+		siape VARCHAR(50),
 		id_turma INT,
-		disciplina STRING,
+		disciplina VARCHAR(100),
 		PRIMARY KEY (id_professor),
 		FOREIGN KEY (id_turma) REFERENCES Turma(id_turma),
-		FOREIGN KEY (id_professor) REFERENCES Professor(id_professor)
+		FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
 );
 
 CREATE TABLE Aluno (
 
 		id_aluno INT NOT NULL,
-		matricula STRING,
+		id_usuario INT,
+		matricula VARCHAR(50),
 		id_turma INT,
-		boletim STRING,
 		horas_estagio FLOAT,
 		PRIMARY KEY (id_aluno),
 		FOREIGN KEY (id_turma) REFERENCES Turma(id_turma),
-		FOREIGN KEY (id_aluno) REFERENCES Aluno(id_aluno)
+		FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
 );
 
 
 CREATE TABLE Secretaria (
 
 		id_secretaria INT NOT NULL,
-		siape STRING,
+		id_usuario INT,	
+		siape VARCHAR(50),
 		PRIMARY KEY (id_secretaria),
-		FOREIGN KEY (id_secretaria) REFERENCES Secretaria(id_secretaria)
+		FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
 );
 
 
 CREATE TABLE Direcao (
 
 		id_diretor INT NOT NULL,
-		siape STRING,
+		id_usuario INT,
+		siape VARCHAR(50),
 		PRIMARY KEY (id_diretor),
-		FOREIGN KEY (id_diretor) REFERENCES Direcao(id_diretor)
+		FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
 );
 
 
-CREATE TABLE SESOP/NAPNE (
+CREATE TABLE SESOPNAPNE (
 
 		id_funcionario INT NOT NULL,
-		siape STRING,
+		id_usuario INT,
+		siape VARCHAR(50),
 		PRIMARY KEY (id_funcionario),
-		FOREIGN KEY (id_funcionario) REFERENCES SESOP/NAPNE(id_funcionario)
+		FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
 );
 
 
