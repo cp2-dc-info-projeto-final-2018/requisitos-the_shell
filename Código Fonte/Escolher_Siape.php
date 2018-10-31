d414c2c03bdf1a38f4153b6a1a44934a29d5cca2
Angel <mjpatinovillamizar@gmail.com>
2018-10-31 08:34:21 -0300

    VAI LOGO VAI LOGO AAAAAA BATATA NAO SABE MEXER NO GIT GRRRR MINHA PACIENCIA GRRR

---
+++ Código Fonte/Escolher_Siape.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>Cadastro</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="Escolher_Siape.css">
+</head>
+<body>
+  <div id="Cabecalho">
+    <h2 id="Nome_do_Colegio">Colégio Pedro II</h2>
+    <h2 id="Nome_do_Software">SHELL - Notas</h2>
+  </div>
+
+  
+<div id="Rodape">
+  <h2>Desenvolvedores</h2>
+  <h4> Carlos Eduardo de França, Danilo Alexandre, Gabriel Rodrigues, João Víctor de Aguiar Nery, Maria Jose.</h4>
+</div>
+
+</body>
+</html>
+<?php
+
+ ?>
