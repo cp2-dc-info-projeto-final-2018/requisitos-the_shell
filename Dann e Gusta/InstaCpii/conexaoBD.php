075ac3018b95878dee83f97fa82a96db52b6325e
leandro <lemccunha@gmail.com>
2018-11-06 08:43:34 -0300

    Inutilia Truncat Corte o inútil - Inútil cortado

--- Dann e Gusta/InstaCpii/conexaoBD.php
+++
@@ -1,7 +0,0 @@
-<?php
-function criaconexaobd() {
-	$bd = new PDO ('mysql:host=localhost;dbname=instaw;charset=utf8', 'instaw', '123');	
-	$bd ->setAttribute (PDO :: ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	return $bd;	
-}  
-?>
--- Dann e Gusta/Programa/conexãoBD.php
+++
@@ -1,8 +0,0 @@
-<?php
-function criaconexaobd() 
-{
-	$bd = new PDO ('mysql:host=localhost;dbname=projetofinal;charset=utf8', 'projetow', '123');
-	$bd ->setAttribute (PDO :: ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	return $bd;
-}
-?>
