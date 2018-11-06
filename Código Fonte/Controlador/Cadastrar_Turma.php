0678a419bf9b810ae301956d0c194e9bbf8abe26
leandro <lemccunha@gmail.com>
2018-11-06 09:59:58 -0300

    Cadastro de Turma Progresso no Cadastro de Turma + Cadastrar_Turma.php criado + Função CadastraTurma criada

---
+++ Código Fonte/Controlador/Cadastrar_Turma.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once('TabelaUsuários.php');
+require_once('Conexão_BD.php');
+require_once('TabelaAlunos.php');
+
+
+?>
---
+++ Código Fonte/Controlador/TabelaTurmas.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once("Conexão_BD.php");
+
+function CadastraTurma($dados)
+{
+  $BD = CriaConexaoBD();
+
+  $SQL = $SQL -> prepare('INSERT INTO turmas(nome, serie) VALUES
+                          (:nome_turma, :serie_turma);');
+
+  $SQL -> bindValue(":nome_turma", $dados["Nome_Turma"]);
+  $SQL -> bindValue(":serie_turma", $dados["Serie_Turma"]);
+
+  $SQL -> execute();
+
+}
+
+
+?>
