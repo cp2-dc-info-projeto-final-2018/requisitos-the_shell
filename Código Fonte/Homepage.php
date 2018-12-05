a5c8cbf41feb1f2bb16d54d5592513fb3df9ca7d
Gustavo <Kishinzzzzz@gmail.com>
2018-12-05 11:08:43 -0200

    HomePage Progresso na HomePage +HomePage está criada (incompleta)

--- Código Fonte/Homepage.php
+++ Código Fonte/Homepage.php
@@ -1,3 +1,9 @@
+<?php
+
+session_start();
+$UsuarioLogado = $_SESSION['Usuário'];
+
+?>
 <!DOCTYPE html>
 
 <html>
@@ -9,21 +15,54 @@
 </head>
 
 <body>
+
+
+<?php if ($Classe_Usuario == 1): ?>
   <div class="Barra_de_Navegacao">
     <a id="SHELL">SHELL</a>
     <a class="Celula" href="#home">Home</a>
     <a class="Celula" href="Controlador/Abrir_Perfil.php">Perfil</a>
-    <a class="Celula" href="Gerenciamento_de_Turmas.php">Turmas</a>
   </div>
+<?php else: ?>
+  <div class="Barra_de_Navegacao">
+    <a id="SHELL">SHELL</a>
+    <a class="Celula" href="#home">Home</a>
+    <a class="Celula" href="Controlador/Abrir_Perfil.php">Perfil</a>
+    <a class="Celula" href="Gerenciamento_de_Turmas.php">Turma</a>
+  </div>
+
+  <?php endif; ?>
+
+
 
   <br>
 
-  <h1>Bem vindo ao SHELL, <!-- [NOME DO USUÁRIO] !--></h1>
+  <h1>Bem vindo ao SHELL, <?= "$UsuarioLogado" ?></h1>
 
-  <div id="Rodape">
-    <h4 class="Desenvolvedores">Desenvolvedores</h4>
-    <p class="Desenvolvedores"> Carlos Eduardo de França, Danilo Alexandre, Gabriel Rodrigues, João Víctor de Aguiar Nery, Maria Jose.</p>
-  </div>
+
+
+
+
+  <div id="Cabecalho" align="center">
+	<h2 id="Nome_do_Software">Home Page</h2>
+	</div>
+
+	<div id="Esquerda">
+		<div id="Caixa_de_Botoes">
+			<link class="Botoes" href="GerenciamentoDeNotas.html" id="btn_Notas" value="Notas"/>
+		</div>
+	</div>
+	<div id="Direita">
+
+
+
+
+	</div>
+
+	<div id="Rodape">
+		<h2>Desenvolvedores</h2>
+		<h4> Carlos Eduardo de França, Danilo Alexandre, Gabriel Rodrigues, João Víctor de Aguiar Nery, Maria Jose.</h4>
+	</div>
 </body>
 
 </html>
