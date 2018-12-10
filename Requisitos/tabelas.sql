ea6197431c2ed933d626d3f6b375a81a37fa3339
NIXYAI <mjpatinovillamizar@gmail.com>
2018-12-10 07:49:08 -0300

    Push Push PUSH PUSH PUSH PUSH

--- Requisitos/tabelas.sql
+++ Requisitos/tabelas.sql
@@ -7,47 +7,47 @@ CREATE TABLE Classe (
 
 CREATE TABLE Disciplina (
 
-		id_disciplina INT NOT NULL AUTO_INCREMENT,
-		disciplina VARCHAR(50),
-		PRIMARY KEY (id_disciplina)
+	id_disciplina INT NOT NULL AUTO_INCREMENT,
+	disciplina VARCHAR(50),
+	PRIMARY KEY (id_disciplina)
 );
 
 CREATE TABLE Turma (
 
-		id_turma INT NOT NULL AUTO_INCREMENT,
-		nome VARCHAR(100),
-		serie VARCHAR(50),
-		integrado BOOLEAN,
-		PRIMARY KEY (id_turma)
+	id_turma INT NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(100),
+	serie VARCHAR(50),
+	integrado BOOLEAN,
+	PRIMARY KEY (id_turma)
 );
 
 CREATE TABLE Usuario (
 
-		id_usuario INT NOT NULL AUTO_INCREMENT,
-		id_classe_usuario INT,
-		login VARCHAR(100),
-		nome VARCHAR(100),
-		senha VARCHAR(20),
-		email VARCHAR(100),
-		tel VARCHAR(50),
-		data_nasc DATE,
-		id_turma INT,
-		PRIMARY KEY (id_usuario),
-		FOREIGN KEY (id_classe_usuario) REFERENCES Classe(id_classe),
-		FOREIGN KEY (id_turma) REFERENCES Turma(id_turma)
+	id_usuario INT NOT NULL AUTO_INCREMENT,
+	id_classe_usuario INT,
+	login VARCHAR(100),
+	nome VARCHAR(100),
+	senha VARCHAR(20),
+	email VARCHAR(100),
+	tel VARCHAR(50),
+	data_nasc DATE,
+	id_turma INT,
+	PRIMARY KEY (id_usuario),
+	FOREIGN KEY (id_classe_usuario) REFERENCES Classe(id_classe),
+	FOREIGN KEY (id_turma) REFERENCES Turma(id_turma)
 );
 
 
 CREATE TABLE Professor (
 
-		id_professor INT,
-		id_classe_usuario INT,
-		siape VARCHAR(50),
-		id_disciplina INT NOT NULL,
-		PRIMARY KEY (id_professor),
-		FOREIGN KEY (id_disciplina) REFERENCES Disciplina(id_disciplina),
-		FOREIGN KEY (id_classe_usuario) REFERENCES Classe(id_classe),
-		FOREIGN KEY (id_professor) REFERENCES Usuario(id_usuario)
+	id_professor INT,
+	id_classe_usuario INT,
+	siape VARCHAR(50),
+	id_disciplina INT NOT NULL,
+	PRIMARY KEY (id_professor),
+	FOREIGN KEY (id_disciplina) REFERENCES Disciplina(id_disciplina),
+	FOREIGN KEY (id_classe_usuario) REFERENCES Classe(id_classe),
+	FOREIGN KEY (id_professor) REFERENCES Usuario(id_usuario)
 );
 
 CREATE TABLE Aluno (
@@ -79,12 +79,12 @@ CREATE TABLE Boletim (
 
 CREATE TABLE Secretaria (
 
-		id_secretaria INT,
-		id_classe_usuario INT,
-		siape VARCHAR(50),
-		PRIMARY KEY (id_secretaria),
-		FOREIGN KEY (id_classe_usuario) REFERENCES Classe(id_classe),
-		FOREIGN KEY (id_secretaria) REFERENCES Usuario(id_usuario)
+	id_secretaria INT,
+	id_classe_usuario INT,
+	siape VARCHAR(50),
+	PRIMARY KEY (id_secretaria),
+	FOREIGN KEY (id_classe_usuario) REFERENCES Classe(id_classe),
+	FOREIGN KEY (id_secretaria) REFERENCES Usuario(id_usuario)
 );
 
 INSERT INTO Classe(classe) VALUES
