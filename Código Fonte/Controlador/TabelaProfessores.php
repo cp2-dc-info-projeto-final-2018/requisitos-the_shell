9eb94bc601b48073cd4be513872dea9bcbb50b81
NIXYAI <mjpatinovillamizar@gmail.com>
2018-12-11 13:02:05 -0300

    tabela prof modificada

--- Código Fonte/Controlador/TabelaProfessores.php
+++ Código Fonte/Controlador/TabelaProfessores.php
@@ -6,7 +6,7 @@ function CadastraProfessor($ID_Usuario, $dadosNovoProfessor)
 {
   $BD = CriaConexaoBD();
 
-  $SQL = $BD -> prepare('INSERT INTO professor(id_usuario, siape, id_classe_usuario,id_disciplina) VALUES
+  $SQL = $BD -> prepare('INSERT INTO professor(id_professor, siape, id_classe_usuario,id_disciplina) VALUES
                          (:id, :siape, 2,:id_disciplina);');
 
   $SQL -> bindValue(':id', $ID_Usuario);
